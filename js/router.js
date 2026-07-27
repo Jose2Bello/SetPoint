753b4f9f72c83ded0b75171c1b2dd2c08b9ee0c7
José Bello <josebelloandreslopez@gmail.com>
2026-07-27 19:18:13 -0400

    feat(teams): Added team view to the page althought the DB was giving some problems on saving the image for the shield of the team, should be carefull for next updates

--- js/router.js
+++ js/router.js
@@ -1,11 +1,12 @@
 import { renderDashboard } from './views/dashboard.view.js';
 import { renderLeagues } from './views/leagues.view.js';
+import { renderTeams } from './views/teams.view.js'; 
 
 const routes = {
     'dashboard': renderDashboard, 
     'leagues': renderLeagues,
-    'teams': null,
-    'team': null,     // expects parameter :id
+    'teams': renderTeams, 
+    'team': null,     
     'players': null,
     'player': null,   // expects parameter :id
     'matches': null,
